4630a927587cdf2e898789a53ac29b93cd6457b0
Daniel Correia <danielsqc@gmail.com>
2022-03-02 23:39:55 -0500

    Added echo function

--- SlackBot/echo-bot.py
+++ SlackBot/echo-bot.py
@@ -4,24 +4,23 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
+load_dotenv()
+print(os.environ['SIGNING_SECRET'])
 app = Flask(__name__)
+
 # Object to handle the events
-slack_event_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'],'/slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
 
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
-# client.chat_postMessage(channel='#test-echo-bot', text='Hello')
 
 BOT_ID = client.api_call('auth.test')['user_id']
 
 @slack_event_adapter.on('message')
 def message(payload):
+    # print(payload)
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
@@ -30,7 +29,15 @@ def message(payload):
         client.chat_postMessage(channel=channel_id, text=text)
     
     
+@app.route('/command/echo', methods=['POST'])
+def echo():
+    data = request.form
+    # print(data)
+    channel_id = data.get('channel_id')
+    text = data.get('text')
+    client.chat_postMessage(channel=channel_id, text=text)
 
+    return Response(), 200
 
 
 if __name__ == "__main__":
