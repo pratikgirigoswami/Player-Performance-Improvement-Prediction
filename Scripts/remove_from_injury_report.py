1f3ddf026228a5213f3be7f1ebeeeb44202f68aa
Daniel Correia <danielsqc@gmail.com>
2022-04-04 19:33:29 -0400

    Included try/except statements

--- Scripts/remove_from_injury_report.py
+++ Scripts/remove_from_injury_report.py
@@ -1,16 +1,21 @@
 # Import the library
 import pickle
+import sys
 
 # Path to the Pickle files
 path_model_input = 'G:/My Drive/Colab Notebooks/00 - Lambton/2022.1/04 - AML3406 - AI and ML Capstone Project/GitHub/Player-Performance-Improvement-Prediction/Scripts/model_input.pkl'
 path_injury_report = 'G:/My Drive/Colab Notebooks/00 - Lambton/2022.1/04 - AML3406 - AI and ML Capstone Project/GitHub/Player-Performance-Improvement-Prediction/Scripts/injury_report.pkl'
 
 # Load the data
-with open(path_model_input, 'rb') as f:
-    model_input = pickle.load(f)
+try:
+    with open(path_model_input, 'rb') as f:
+        model_input = pickle.load(f)
 
-with open(path_injury_report, 'rb') as f:
-    injury_report = pickle.load(f)
+    with open(path_injury_report, 'rb') as f:
+        injury_report = pickle.load(f)
+except:
+    print('There was an error loading the data')
+    sys.exit()
 
 # Adjust injury report format
 injury_report = injury_report.split(', ')
@@ -25,9 +30,16 @@ model_input['Injury'] = model_input['Name lower'].apply(lambda x: 1 if x in inju
 # DataFrame with only non injuried players
 new_model_input = model_input[model_input['Injury'] == 0].copy()
 
+if new_model_input.shape[0] == 0:
+    print('Error! No players in the model_input file!')
+    sys.exit()
+
 # Drop the auxiliaries columns
 new_model_input.drop(['Name lower', 'Injury'], axis=1, inplace=True)
 
 # Save the filtered data
-with open(new_model_input, 'wb') as f:
-    pickle.dump(f, path_model_input)
+try:
+    with open(path_model_input, 'wb') as f:
+        pickle.dump(path_model_input, f)
+except:
+    print('Error saving the model_input file.')
