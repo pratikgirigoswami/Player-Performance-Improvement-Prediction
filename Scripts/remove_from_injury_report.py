fe8d726e3ae22d23fde739c8344578f3fa608d76
Daniel Correia <danielsqc@gmail.com>
2022-04-04 20:50:02 -0400

    Input with more recent data only

--- Scripts/remove_from_injury_report.py
+++ Scripts/remove_from_injury_report.py
@@ -1,9 +1,11 @@
 # Import the library
 import pickle
 import sys
+import pandas as pd
 
 # Path to the Pickle files
 path_model_input = 'G:/My Drive/Colab Notebooks/00 - Lambton/2022.1/04 - AML3406 - AI and ML Capstone Project/GitHub/Player-Performance-Improvement-Prediction/Scripts/model_input.pkl'
+path_filtered_input = 'G:/My Drive/Colab Notebooks/00 - Lambton/2022.1/04 - AML3406 - AI and ML Capstone Project/GitHub/Player-Performance-Improvement-Prediction/Scripts/filtered_model_input.pkl'
 path_injury_report = 'G:/My Drive/Colab Notebooks/00 - Lambton/2022.1/04 - AML3406 - AI and ML Capstone Project/GitHub/Player-Performance-Improvement-Prediction/Scripts/injury_report.pkl'
 
 # Load the data
@@ -37,9 +39,18 @@ if new_model_input.shape[0] == 0:
 # Drop the auxiliaries columns
 new_model_input.drop(['Name lower', 'Injury'], axis=1, inplace=True)
 
+# Keep only the more recent data
+players_id = new_model_input['Player ID'].unique()
+
+data = []
+for id in players_id:
+    data.append(new_model_input[new_model_input['Player ID'] == id].sort_values('Game Date', ascending=False).iloc[0])
+
+recent_data = pd.DataFrame(data)
+
 # Save the filtered data
 try:
-    with open(path_model_input, 'wb') as f:
-        pickle.dump(new_model_input, f)
+    with open(path_filtered_input, 'wb') as f:
+        pickle.dump(recent_data, f)
 except:
     print('Error saving the model_input file.')
